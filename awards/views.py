95827dcd43c9ae57275b82b8f4560488e2da37b0
TERESIA012 <kingoriteresia@gmail.com>
2021-10-22 15:59:01 +0300

    add function edit profile

--- awards/views.py
+++ awards/views.py
@@ -71,3 +71,7 @@ def addProject(request):
     else:
         form = projectForm()
     return render(request,'newProject.html',{'form':form}) 
+
+def profile(request,id):
+    prof = Profile.objects.get(user = id)
+    return render(request,'profile.html',{"profile":prof})
