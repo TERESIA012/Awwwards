6bfa75635d72da5547372b0e17cfa9e121eb5720
TERESIA012 <kingoriteresia@gmail.com>
2021-10-22 15:18:27 +0300

    add function index

--- awards/views.py
+++ awards/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Profile,Projects,Review
 
 # Create your views here.
+
+def index(request):
+    projects = Projects.objects.all()
+    return render(request,'index.html',{"projects":projects})
