26d985590fb7ac39883c8a203cc99efe43ab8c3a
TERESIA012 <kingoriteresia@gmail.com>
2021-10-22 15:23:15 +0300

    add userupdate profile

--- awards/forms.py
+++ awards/forms.py
@@ -13,4 +13,10 @@ class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
     class Meta:
         model = User
-        fields = ['username','email']         
+        fields = ['username','email'] 
+        
+class UpdateUserProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['Bio', 'photo']        
+                
