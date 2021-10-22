9313ba566598b1308cdc98679abcac1fd765ff52
TERESIA012 <kingoriteresia@gmail.com>
2021-10-22 15:13:35 +0300

    add tests

--- awards/tests.py
+++ awards/tests.py
@@ -1,3 +1,40 @@
+from unittest.suite import TestSuite
 from django.test import TestCase
+from .models import Profile,Projects,Image,Self
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class ProfileTest(TestCase):
+    def setUp(self):
+        self.tess = User(username = 'Tess',email = 'kingoriteresia@gmail.com')
+        self.tess = Profile(user = Self.tess,user = 1,Bio = 'tests',photo = 'test.jpg',date_created='oct,22.2021')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.tess,Profile))
+
+    def test_save_profile(self):
+        Profile.save_profile(self)
+        all_profiles = Profile.objects.all()
+        self.assertTrue(len(all_profiles),0)
+
+    def test_delete_profile(self):
+        self.tess.delete_profile()
+        all_profiles = Profile.objects.all()
+        self.assertEqual(len(all_profiles),0)
+
+
+
+class ProjectsTestCase(TestCase):
+    def setUp(self):
+        self.new_post = Projects(title = 'testT',projectscreenshot = 'test.jpg',description = 'testD',user = TestSuite,projecturl = 'https://test.com',datecreated='Dec,22.2021')
+
+
+    def test_save_project(self):
+        self.new_post.save_project()
+        pictures = Image.objects.all()
+        self.assertEqual(len(pictures),1)
+
+    def test_delete_project(self):
+        self.new_post.delete_project()
+        pictures = Projects.objects.all()
+        self.assertEqual(len(pictures),1)
